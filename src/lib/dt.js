2b37359d42e91e3c10debb89ad770aab202eb59c
Evan Pearson <parsnbl@icloud.com>
2023-10-21 15:56:56 -0700

    init commit

---
+++ src/lib/dt.js
@@ -0,0 +1,38 @@
+
+class DateTime extends Date {
+  constructor(...args) {
+    super(...args)
+  }
+  getDaysFromYearStart = () => {
+    const yearStart = new Date(this.getFullYear(), 0 , 1);
+    return Math.floor((this - yearStart) / (24 * 60 * 60 * 1000)) + 1;
+  }
+  getWeekNumber = () => Math.ceil(this.getDaysfromYearStart() / 7);
+  addDays = (days) => {
+    const daysFromStart = this.getDaysFromYearStart()
+    this.setMonth(0);
+    this.setDate(1);
+    this.setDate(daysFromStart + days)
+    return this;
+  }
+  addTime = (hours, minutes, seconds, ms) => {
+    hours = hours !== undefined ? hours : 0;
+    minutes = minutes !== undefined ? minutes : 0;
+    seconds = seconds !== undefined ? seconds : 0;
+    ms = ms !== undefined ? seconds : 0;
+    const origHours = this.getHours();
+    const origMinutes = this.getMinutes();
+    const origSeconds = this.getSeconds();
+    const origMs = this.getMilliseconds();
+    this.setHours(
+      hours+origHours, 
+      minutes+origMinutes, 
+      seconds+origSeconds, 
+      ms+origMs
+      );
+    return this;
+  }
+
+}
+
+export default DateTime
